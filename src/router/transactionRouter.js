a1678d16d01fab77a1f76e93256e459cd759f8d4
samikshya <samikshyakharel405@gmail.com>
2024-05-09 16:06:19 +1000

    bug fixed

--- src/router/transactionRouter.js
+++ src/router/transactionRouter.js
@@ -1,71 +1,68 @@
 import express from "express";
-import {  deleteTransaction, getTransactionById, insertNewTrans } from "../model/transaction/TransactionModel.js";
+import {
+  deleteTransaction,
+  getTransactionById,
+  insertNewTrans,
+} from "../model/transaction/transactionModel.js";
 
 const router = express.Router();
 
-
-
-router.get("/", async(req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const trans = await getTransactionById(req.body)
+    const trans = await getTransactionById(req.body);
 
     res.json({
       status: "success",
       message: "here are the list of the transaction",
-      trans
+      trans,
     });
   } catch (error) {
     res.status(500).json({
-      status:"error",
-      message:error
-    })
+      status: "error",
+      message: error,
+    });
   }
 });
-router.post("/", async(req, res) => {
-  const {authorization} = req.headers
+router.post("/", async (req, res) => {
+  const { authorization } = req.headers;
   try {
-    
-    const result = await insertNewTrans({...req.body, userId:authorization})
-    result?._id ? 
-    res.json({
-      status: "success",
-      message: "new transaction has been added",
-    }):
-    res.json({
-      status:"error",
-      message:"unable to process your request"
-    })
+    const result = await insertNewTrans({ ...req.body, userId: authorization });
+    result?._id
+      ? res.json({
+          status: "success",
+          message: "new transaction has been added",
+        })
+      : res.json({
+          status: "error",
+          message: "unable to process your request",
+        });
   } catch (error) {
     res.status(500).json({
-      status:"error",
-      message:error
-    })
+      status: "error",
+      message: error,
+    });
   }
 });
 
-router.delete("/",async(req,res)=>{
-    
-    try {
-        const {ids} = req.body;
+router.delete("/", async (req, res) => {
+  try {
+    const { ids } = req.body;
 
-        const result = await deleteTransaction(ids)
-        console.log(result)
-        
+    const result = await deleteTransaction(ids);
+    console.log(result);
 
-        result?.deletedCount>0
+    result?.deletedCount > 0
       ? res.json({
           message: "task has been deleted",
         })
       : res.json({
           message: "unable to delete try again later",
         });
-
-    } catch (error) {
-        res.status(500).json({
-            status: "error",
-            message: "Something went wrong in server. Please contact the provider.",
-          });
-        
-    }
-})
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong in server. Please contact the provider.",
+    });
+  }
+});
 export default router;
