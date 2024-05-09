e242b44e07dc5f6cdd90846a792cd7f1d79a3e75
samikshya <samikshyakharel405@gmail.com>
2024-05-09 16:01:44 +1000

    mongodb chnge

--- src/config/connectMongo.js
+++ src/config/connectMongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectMongo = () => {
   try {
-    const con = mongoose.connect(process.env.MONGO_URL);
+    const con = mongoose.connect(process.env.MONGODB_URL);
     con && console.log("db Connected");
   } catch (error) {
     console.log(error);
